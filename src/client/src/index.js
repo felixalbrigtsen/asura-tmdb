2c6f28e6da4c96da0e0b77b72e9f4d3c6dfdb63b
Kristoffer Longva Eriksen <krisleri@stud.ntnu.no>
2022-03-15 12:38:38 +0100

    Initial client build

---
+++ src/client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Hello from "./front_page";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
