7eb631fd2152aad9c517cb7d6c3a9cfe1049c3f0
Felix Albrigtsen <felixalbrigtsen@gmail.com>
2022-03-16 15:40:33 +0100

    Improved database interface

--- src/server/index.js
+++ src/server/index.js
@@ -23,42 +23,60 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "landing.html"));
 });
 
-app.get("/getMatches", (req, res) => {
-  connection.query("SELECT * FROM matches", (err, matches) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(matches);
-    }
+app.get("/tournament/:tournamentId/getMatches", (req, res) => {
+  let tournamentId = req.params.tournamentId;
+  getMatchesByTournamentId(tournamentId)
+    .then((result) => res.send({"status": "OK", "data": result}))
+    .catch((err) => res.send({"status": "ERROR", "data": err}));
+});
+
+// app.get("/getMatches", (req, res) => {
+//   connection.query("SELECT * FROM matches", (err, matches) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(matches);
+//     }
+//   });
+// });
+
+
+// let tournaments = {
+//   "1": {
+//     "name": "Tournament 1",
+//     "description": "This is the first tournament",
+//     "matches":[
+//       {"id": "2",
+//       "player1": "Player 1",
+//       "player2": "Player 2",
+//       "winner": "Player 1",
+//     }
+//     ]
+//   },
+//   "2": { 
+//     "name": "Tournament 2",
+//     "description": "This is the second tournament",
+//     "matches":[
+//         {"id": "2",
+//         "player1": "Player 1",
+//         "player2": "Player 2",
+//         "winner": "Player 1",
+//       }]
+//     }
+// };
+// app.get("/tournament/:tournamentId", (req, res) => {
+//   res.render(path.join(__dirname, "public", "tournament.html"), {"tournament":tournaments[req.params.tournamentId]});
+// });
+
+function getMatchesByTournamentId(tournamentId) {
+  return new Promise(function(resolve, reject) {
+    connection.query("SELECT * FROM matches WHERE tournament_id = ?", [mysql.escape(tournamentId)], (err, matches) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(matches);
+      }
+    });
   });
-});
-
-
-let tournaments = {
-  "1": {
-    "name": "Tournament 1",
-    "description": "This is the first tournament",
-    "matches":[
-      {"id": "2",
-      "player1": "Player 1",
-      "player2": "Player 2",
-      "winner": "Player 1",
-    }
-    ]
-  },
-  "2": { 
-    "name": "Tournament 2",
-    "description": "This is the second tournament",
-    "matches":[
-        {"id": "2",
-        "player1": "Player 1",
-        "player2": "Player 2",
-        "winner": "Player 1",
-      }]
-    }
-};
-app.get("/tournament/:tournamentId", (req, res) => {
-  res.render(path.join(__dirname, "public", "tournament.html"), {"tournament":tournaments[req.params.tournamentId]});
-});
-
-
+}
