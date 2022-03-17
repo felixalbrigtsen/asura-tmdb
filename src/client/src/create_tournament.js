0ba7fa779f0881a4a910aa331c657044c2ecc750
Kristoffer Longva Eriksen <krisleri@stud.ntnu.no>
2022-03-17 14:45:36 +0100

    Added format selector to Create Tournament page

--- src/client/src/create_tournament.js
+++ src/client/src/create_tournament.js
@@ -2,6 +2,17 @@ import * as React from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import Home_Image from "./components/home_image";
 
+function Format_Selector(props) {
+  return (
+    <div>
+      <select>
+        <option value="Single Elimination">Single Elimination</option>
+        <option value="Double Elimination">Double Elimination</option>
+      </select>
+    </div>
+  );
+}
+
 function Save_Button(props) {
   return (
     <Link to="/">
@@ -14,6 +25,7 @@ export default function Create_Tournament(props) {
   return (
     <React.Fragment>
       <Home_Image />
+      <Format_Selector />
       <Save_Button />
     </React.Fragment>
   );
