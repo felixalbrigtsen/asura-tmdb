868f080cb6da63218198bff6dc64f36e48369896
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-03-23 18:51:03 +0100

    added themeprovider to entire page

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./frontpage.js";
+import theme from './components/theme';
+import { ThemeProvider } from "@emotion/react";
 
 ReactDOM.render(
   <>
+  <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </>,
   document.getElementById("root")
 );
