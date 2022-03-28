f2e30593948c2a50308d063d85468fffa6ed10ad
Felix Albrigtsen <felixalbrigtsen@gmail.com>
2022-03-28 19:46:39 +0200

    header fix

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ app.use("/api", api);
 
 api.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 api.use(require('express-log-url'));
